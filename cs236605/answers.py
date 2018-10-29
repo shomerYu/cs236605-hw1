c6f6ec0f26701759110b34596957810000ec1487
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-10-29 22:57:03 +0200

    Re-generated from the main repo.

--- cs236605/answers.py
+++ cs236605/answers.py
@@ -23,12 +23,12 @@ ANSWER_BLOCK_PATTERN = \
 SOLUTION_BLOCK_REPLACEMENT = r"raise NotImplementedError()"
 
 ANSWER_BLOCK_REPLACEMENT = r"""
-Write your answer using **markdown** and $\LaTeX$:
+Write your answer using **markdown** and $\\LaTeX$:
 ```python
 # A code block
 a = 2
 ```
-An equation: $e^{i\pi} -1 = 0$
+An equation: $e^{i\\pi} -1 = 0$
 """
 
 
